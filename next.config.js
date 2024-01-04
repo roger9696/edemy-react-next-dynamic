9edac1cd342f7c1f5ad9f3d6baac2661e5b08433
Shabbir Ahmed <shabbir@salahsoftwaresolution.com>
2024-01-04 10:01:44 +0600

    - Configure dependencies

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	images: {
+		unoptimized: true,
+		domains: [
+			"res.cloudinary.com",
+			"avatars.githubusercontent.com",
+			"lh3.googleusercontent.com",
+			"via.placeholder.com",
+			"developers.google.com",
+		],
+	},
+	env: {
+		NEXTAUTH_SECRET: "b51afa1ed38dde0d5d8f21gftybjsygg458086548a7fe48e1a",
+		NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: "dev-empty",
+		NEXT_CLOUDINARY_PRESET: "vikings",
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
